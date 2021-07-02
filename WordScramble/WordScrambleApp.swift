ba0657c0c92ab616381f4eff48e9c34966711b2b
Caio <caiofmart@gmail.com>
2021-07-02 14:50:30 -0300

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Caio Feitoza Martins on 02/07/21.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
